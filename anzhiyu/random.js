98040b878b4f841560b81e20e9b4b5dfd1ac42b7
Melody <Melody5801@foxmail.com>
2024-02-04 18:21:03 +0800

    Site updated: 2024-02-04 18:21:02

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/26/HarmonyOS 开发/","2024/01/01/Java-ArrayList/","2023/02/01/毕业设计文档/","2023/12/27/Java-Object/","2023/12/27/MySQL 数据库/","2023/12/29/Java-String/"];function toRandomPost(){
+var posts=["2023/12/26/HarmonyOS 开发/","2023/12/29/Java-String/","2024/01/01/Java-ArrayList/","2023/12/27/MySQL 数据库/","2023/02/01/毕业设计文档/","2023/12/27/Java-Object/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
